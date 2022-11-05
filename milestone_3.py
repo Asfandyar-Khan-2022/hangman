4e8dbb2d1c47b6896186ca1b33e31a285bb12ecf
kickass2k14 <asfandyarkhan@spartaglobal.com>
2022-11-05 01:30:42 +0000

    your commit message

---
+++ milestone_3.py
@@ -0,0 +1,16 @@
+secret_word = 'apple'
+
+def ask_for_input():
+    guess = input('Guess a letter: ')
+    while len(guess) != 1 or guess.isalpha() != True:
+        guess = input('Please only write a single letter: ').lower()
+    return guess
+
+def check_guess(guess):
+    if guess in secret_word:
+        print(f'Good guess! {guess} is in the word')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again')
+
+check_guess(ask_for_input())
+
